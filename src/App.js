d82c89efaa26150a042c68c75c6609ff0b42eb3c
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2023-03-20 16:46:57 +0100

    add some animation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import {Container} from "react-bootstrap"
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Parallax } from 'react-parallax';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // initialize AOS library
+    React.useEffect(() => {
+        AOS.init();
+    }, []);
+
+    return (
+        <div className="App">
+            <Parallax bgImage={'/path/to/background/image.jpg'} strength={500}>
+                <Container>
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <h1 data-aos="fade-up" data-aos-duration="1000">
+                            Super Crazy Baby
+                        </h1>
+                        <a
+                            className="App-link"
+                            href="https://reactjs.org"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500"
+                        >
+                            Learn React
+                        </a>
+                    </header>
+                </Container>
+            </Parallax>
+        </div>
+    );
 }
 
 export default App;
